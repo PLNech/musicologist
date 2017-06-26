bbdd76f20ca8400891513c9fb90ba6fed99481a0
Paul-Louis NECH <pln@algolia.com>
2017-06-26 23:22:48 +0200

    Search: Implement search by time interval

--- search.py
+++ search.py
@@ -1,6 +1,10 @@
+#! /usr/bin/env python3
+import datetime
 import json
 import uuid
 import sys
+from dateutil.parser import parse
+
 
 from apis import index, ai
 
@@ -23,9 +27,27 @@ action = result['action']
 
 if action == "search.artist":
     artist = result['parameters']['artist']
-    print("Searching for %s..." % artist)
-    hits = index.search(artist  )['hits']
-    songs = [hit['trackName'] for hit in hits]
+    period = result['parameters']['period']
+    period_original = result['contexts'][0]['parameters']['period.original']
+    hits = {}
+    songs = []
+    if artist and not period:
+        print("Searching for %s..." % artist)
+        hits = index.search(artist)['hits']
+        songs = [hit['trackName'] for hit in hits]
+    elif period and not artist:
+        (start, end) = period.split("/")
+        date_start = parse(start)
+        date_end = parse(end)
+        print("Period: %s to %s" % (start, end))
+        print("Period: %s to %s" % (date_start, date_end))
+        print("Searching for songs %s..." % period_original)
+        filters = 'release_timestamp: ' + str(int(date_start.timestamp())) + ' TO ' + str(int(date_end.timestamp()))
+        print("filters: %s" % filters)
+        hits = index.search(artist, {'filters': filters})['hits']
+        songs = [hit['trackName'] for hit in hits]
+    else:
+        print("NOTHING!!1! (%s)" % result['parameters'])
     print("Found %s songs: %s." % (len(hits), ", ".join(songs)))
 else:
     print("Weird action: %s." % result['action'])
