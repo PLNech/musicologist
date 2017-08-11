91975a969b4e4ef0a9ff33270ebc46d56a984388
Paul-Louis NECH <pln@algolia.com>
2017-08-11 18:24:01 +0200

    apis: Use million_songs

--- apis.py
+++ apis.py
@@ -7,7 +7,7 @@ from algoliasearch import algoliasearch
 app_id = os.environ.get("ALGOLIA_APPLICATION_ID")
 api_key = os.environ.get("ALGOLIA_API_KEY")
 client = algoliasearch.Client(app_id, api_key)
-index = client.init_index("songs")
+index = client.init_index("million_songs")
 
 # API.AI
 token_dev = '01df5cae360044deb39081f3d7a6bc1e'
