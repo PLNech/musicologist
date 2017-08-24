8b74e5943fb8a40ce3d5790372a6ea36d0064533
Paul-Louis NECH <pln@algolia.com>
2017-08-24 17:51:38 +0200

    MainActivity: Request audio permission

--- android/app/src/main/java/com/algolia/musicologist/MainActivity.java
+++ android/app/src/main/java/com/algolia/musicologist/MainActivity.java
@@ -1,11 +1,16 @@
 package com.algolia.musicologist;
 
+import android.Manifest;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.speech.RecognizerIntent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -15,8 +20,11 @@ import android.widget.Toast;
 
 import java.util.List;
 
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+
 public class MainActivity extends AppCompatActivity {
-    private static final int SPEECH_REQUEST_CODE = 0;
+    private static final int CODE_SPEECH_REQUEST = 0;
+    private static final int CODE_PERMISSION_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        requestAudioPermission();
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,13 +43,50 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CODE_PERMISSION_REQUEST) {
+            if (grantResults.length > 0 && grantResults[0] == PERMISSION_GRANTED) {
+                Toast.makeText(this, "Thanks! Looking forward to hearing your lovely voice.", Toast.LENGTH_LONG).show();
+            } else {
+                requestAudioPermission();
+            }
+        }
+    }
+
+    private void requestAudioPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+                != PERMISSION_GRANTED) {
+            int delay = 0;
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.RECORD_AUDIO)) {
+
+                Toast.makeText(this, "This app only works if you allow it to record audio.", Toast.LENGTH_SHORT).show();
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                delay = 1000;
+            }
+            // Explanation given or no explanation needed, we can request the permission.
+            new Handler().postDelayed(new Runnable() {
+                @Override public void run() {
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.RECORD_AUDIO},
+                            CODE_PERMISSION_REQUEST);
+                }
+            }, delay);
+        }
+    }
 
     // Create an intent that can start the Speech Recognizer activity
     private void displaySpeechRecognizer() {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         try {
-            startActivityForResult(intent, SPEECH_REQUEST_CODE);
+            startActivityForResult(intent, CODE_SPEECH_REQUEST);
         } catch (ActivityNotFoundException e) {
             Toast.makeText(this, "You need at least a Voice Recognition Provider, such as Google Quick Search Box.", Toast.LENGTH_LONG).show();
             String appPackageName = "com.google.android.googlequicksearchbox";
@@ -57,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode,
                                     Intent data) {
-        if (requestCode == SPEECH_REQUEST_CODE) {
+        if (requestCode == CODE_SPEECH_REQUEST) {
             if (resultCode == RESULT_OK) {
                 List<String> results = data.getStringArrayListExtra(
                         RecognizerIntent.EXTRA_RESULTS);
