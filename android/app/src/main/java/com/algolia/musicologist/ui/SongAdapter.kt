8efd0c7f22ed98b79000e0f17a937b011e35f6b2
Paul-Louis NECH <pln@algolia.com>
2017-09-01 10:55:01 +0200

    refactor(renderHighlights): Merge in SongAdapter

--- android/app/src/main/java/com/algolia/musicologist/ui/SongAdapter.kt
+++ android/app/src/main/java/com/algolia/musicologist/ui/SongAdapter.kt
@@ -2,6 +2,10 @@ package com.algolia.musicologist
 
 import android.content.Context
 import android.os.Build
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.BackgroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,10 +16,10 @@ import com.nostra13.universalimageloader.core.ImageLoader
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration
 import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer
 import kotlinx.android.synthetic.main.cell_song.view.*
+import java.util.regex.Pattern
 
 internal class SongAdapter(context: Context, resource: Int) : ArrayAdapter<HighlightedResult<Song>>(context, resource) {
 
-    private val highlightRenderer: HighlightRenderer
     private val imageLoader: ImageLoader = ImageLoader.getInstance()
     private val displayImageOptions: DisplayImageOptions = DisplayImageOptions.Builder()
             .cacheOnDisk(true)
@@ -36,7 +40,6 @@ internal class SongAdapter(context: Context, resource: Int) : ArrayAdapter<Highl
             }
         }).start()
 
-        highlightRenderer = HighlightRenderer(context)
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
@@ -44,7 +47,7 @@ internal class SongAdapter(context: Context, resource: Int) : ArrayAdapter<Highl
         val result = getItem(position)
 
         imageLoader.displayImage(result!!.result.trackViewUrl, cell.preview, displayImageOptions)
-        cell.title.text = highlightRenderer.renderHighlights(result[Song.TITLE]?.highlightedValue)
+        cell.title.text = renderHighlights(result[Song.TITLE]?.highlightedValue)
         cell.release.text = String.format("%d", result.result.release_timestamp)
 
         return cell
@@ -62,4 +65,34 @@ internal class SongAdapter(context: Context, resource: Int) : ArrayAdapter<Highl
             }
         }
     }
+
+    private fun renderHighlights(markupString: String?): Spannable {
+        val result = SpannableStringBuilder()
+        markupString?.let {
+            val matcher = HIGHLIGHT_PATTERN.matcher(markupString)
+            var p = 0 // current position in input string
+            var q = 0 // current position in output string
+            // For each highlight...
+            while (matcher.find()) {
+                // Append text before.
+                result.append(markupString.substring(p, matcher.start()))
+                q += matcher.start() - p
+
+                // Append highlighted text.
+                val highlightString = matcher.group(1)
+                result.append(highlightString)
+                result.setSpan(BackgroundColorSpan(context.resources.getColor(R.color.colorAccent)), q, q + highlightString.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+                q += highlightString.length
+                p = matcher.end()
+            }
+            // Append text after.
+            result.append(markupString.substring(p))
+        }
+        return result
+    }
+
+    companion object {
+        internal val HIGHLIGHT_PATTERN = Pattern.compile("<em>([^<]*)</em>")
+    }
+
 }
